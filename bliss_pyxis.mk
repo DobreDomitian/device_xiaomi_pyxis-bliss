c2a0c0a295cb2c71d2337b615152e13498b8aaa5
NaassomDourado <naassomdourado@gmail.com>
2020-12-05 10:37:54 -0300

    Blissify

--- bliss_pyxis.mk
+++ bliss_pyxis.mk
@@ -1,19 +1,23 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 Bliss Team
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from pyxis device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := pyxis
-PRODUCT_NAME := arrow_pyxis
+PRODUCT_NAME := bliss_pyxis
 PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 9 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
